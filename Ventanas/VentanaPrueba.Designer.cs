921d36e2b294929dba14223fc2d750f6bdf6ffe5
roi-utn-08 <riodri25@gmail.com>
2022-05-07 11:28:23 -0300

    Update VentanaPrueba.Designer.cs

--- Ventanas/VentanaPrueba.Designer.cs
+++ Ventanas/VentanaPrueba.Designer.cs
@@ -32,16 +32,16 @@
             this.panelBarra = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
             this.MenuVertical = new System.Windows.Forms.Panel();
-            this.btn_menu = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btn = new FontAwesome.Sharp.IconButton();
+            this.iconButton3 = new FontAwesome.Sharp.IconButton();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.btn_Inicio = new FontAwesome.Sharp.IconButton();
             this.Logo = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel8 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelBarra.SuspendLayout();
             this.MenuVertical.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelBarra
@@ -73,101 +73,113 @@
             // MenuVertical
             // 
             this.MenuVertical.BackColor = System.Drawing.Color.Gray;
-            this.MenuVertical.Controls.Add(this.btn_menu);
-            this.MenuVertical.Controls.Add(this.button5);
-            this.MenuVertical.Controls.Add(this.button4);
-            this.MenuVertical.Controls.Add(this.button3);
-            this.MenuVertical.Controls.Add(this.button2);
+            this.MenuVertical.Controls.Add(this.btn);
+            this.MenuVertical.Controls.Add(this.iconButton3);
+            this.MenuVertical.Controls.Add(this.iconButton2);
+            this.MenuVertical.Controls.Add(this.iconButton1);
+            this.MenuVertical.Controls.Add(this.btn_Inicio);
             this.MenuVertical.Controls.Add(this.Logo);
-            this.MenuVertical.Controls.Add(this.button1);
             this.MenuVertical.Dock = System.Windows.Forms.DockStyle.Left;
             this.MenuVertical.Location = new System.Drawing.Point(0, 30);
             this.MenuVertical.Name = "MenuVertical";
             this.MenuVertical.Size = new System.Drawing.Size(250, 431);
             this.MenuVertical.TabIndex = 1;
             // 
-            // btn_menu
+            // btn
             // 
-            this.btn_menu.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_menu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_menu.BackgroundImage")));
-            this.btn_menu.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_menu.FlatAppearance.BorderSize = 0;
-            this.btn_menu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_menu.Location = new System.Drawing.Point(220, 0);
-            this.btn_menu.Name = "btn_menu";
-            this.btn_menu.Size = new System.Drawing.Size(30, 30);
-            this.btn_menu.TabIndex = 4;
-            this.btn_menu.UseVisualStyleBackColor = true;
-            this.btn_menu.Click += new System.EventHandler(this.btn_menu_Click);
+            this.btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn.FlatAppearance.BorderSize = 0;
+            this.btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn.IconChar = FontAwesome.Sharp.IconChar.Spider;
+            this.btn.IconColor = System.Drawing.Color.Black;
+            this.btn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn.IconSize = 30;
+            this.btn.Location = new System.Drawing.Point(220, 0);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(30, 30);
+            this.btn.TabIndex = 3;
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
             // 
-            // button5
+            // iconButton3
             // 
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Location = new System.Drawing.Point(3, 262);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(244, 30);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.iconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton3.IconChar = FontAwesome.Sharp.IconChar.Cat;
+            this.iconButton3.IconColor = System.Drawing.Color.Black;
+            this.iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton3.IconSize = 30;
+            this.iconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton3.Location = new System.Drawing.Point(3, 231);
+            this.iconButton3.Name = "iconButton3";
+            this.iconButton3.Size = new System.Drawing.Size(244, 30);
+            this.iconButton3.TabIndex = 7;
+            this.iconButton3.Text = "actividad 3";
+            this.iconButton3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // iconButton2
             // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(3, 226);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(244, 30);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.Spider;
+            this.iconButton2.IconColor = System.Drawing.Color.Black;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 30;
+            this.iconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton2.Location = new System.Drawing.Point(3, 195);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(244, 30);
+            this.iconButton2.TabIndex = 6;
+            this.iconButton2.Text = "actividad 2";
+            this.iconButton2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // iconButton1
             // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(3, 190);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(244, 30);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.Hippo;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 30;
+            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.Location = new System.Drawing.Point(3, 159);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(244, 30);
+            this.iconButton1.TabIndex = 5;
+            this.iconButton1.Text = "Actividad 1";
+            this.iconButton1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btn_Inicio
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(3, 154);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(244, 30);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_Inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Inicio.IconChar = FontAwesome.Sharp.IconChar.Home;
+            this.btn_Inicio.IconColor = System.Drawing.Color.Black;
+            this.btn_Inicio.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btn_Inicio.IconSize = 30;
+            this.btn_Inicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Inicio.Location = new System.Drawing.Point(3, 123);
+            this.btn_Inicio.Name = "btn_Inicio";
+            this.btn_Inicio.Size = new System.Drawing.Size(244, 30);
+            this.btn_Inicio.TabIndex = 4;
+            this.btn_Inicio.Text = "Inicio";
+            this.btn_Inicio.UseVisualStyleBackColor = true;
             // 
             // Logo
             // 
             this.Logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Logo.BackgroundImage")));
             this.Logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Logo.Location = new System.Drawing.Point(0, 27);
+            this.Logo.Location = new System.Drawing.Point(0, 32);
             this.Logo.Name = "Logo";
             this.Logo.Size = new System.Drawing.Size(250, 85);
             this.Logo.TabIndex = 2;
             // 
-            // button1
+            // pictureBox1
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(3, 118);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(244, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // panel8
-            // 
-            this.panel8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.panel8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel8.BackgroundImage")));
-            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel8.Location = new System.Drawing.Point(438, 148);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(551, 249);
-            this.panel8.TabIndex = 3;
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(985, 260);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 201);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // VentanaPrueba
             // 
@@ -175,7 +187,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1184, 461);
-            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.MenuVertical);
             this.Controls.Add(this.panelBarra);
             this.Name = "VentanaPrueba";
@@ -183,6 +195,7 @@
             this.Load += new System.EventHandler(this.VentanaPrueba_Load);
             this.panelBarra.ResumeLayout(false);
             this.MenuVertical.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -191,14 +204,13 @@
 
         private System.Windows.Forms.Panel panelBarra;
         private System.Windows.Forms.Panel MenuVertical;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Panel Logo;
-        private System.Windows.Forms.Button btn_menu;
         private System.Windows.Forms.Button button6;
+        private FontAwesome.Sharp.IconButton btn_Inicio;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btn;
     }
 }
