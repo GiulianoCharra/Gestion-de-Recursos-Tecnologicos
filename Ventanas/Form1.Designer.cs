fb9354f0c299734e231bb6cd9fa776e7f8d37004
roi-utn-08 <riodri25@gmail.com>
2022-05-11 10:30:48 -0300

    Update Form1.Designer.cs
    nuevo nombre from

--- Ventanas/Form1.Designer.cs
+++ Ventanas/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Gestión_de_Recursos_Tecnológicos
 {
-    partial class Form1
+    partial class frm_recursoTecnologico
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,14 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(898, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(30, 30);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "X";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // frm_recursoTecnologico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(928, 461);
+            this.Controls.Add(this.button6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_recursoTecnologico";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -43,6 +63,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button6;
     }
 }
 
